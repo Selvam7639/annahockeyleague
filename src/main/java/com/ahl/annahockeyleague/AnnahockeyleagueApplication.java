383ae807d1ab045e02763867e58f2b549b92dfcc
Tamizhselvam Selvam <selvamtamil47@gmail.com>
2022-12-30 00:54:59 +0530

    initial commit

---
+++ src/main/java/com/ahl/annahockeyleague/AnnahockeyleagueApplication.java
@@ -0,0 +1,13 @@
+package com.ahl.annahockeyleague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnahockeyleagueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnahockeyleagueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ahl/annahockeyleague/AnnahockeyleagueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahl.annahockeyleague;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnahockeyleagueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
